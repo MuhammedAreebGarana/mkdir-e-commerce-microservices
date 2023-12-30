152def5bd57ca66d8f98f97e5b5e495c44bf0be7
MuhammedAreeb <135113666+MuhammedAreebGarana@users.noreply.github.com>
2023-12-30 22:24:27 +0500

    Update models.py

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+# catalog_microservice/models.py
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -7,3 +8,25 @@ class Product(db.Model):
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=True)
     price = db.Column(db.Float, nullable=False)
+    
+# user_microservice/models.py
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    # Add other user-related fields as needed
+# order_microservice/models.py
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False)
+    product_id = db.Column(db.Integer, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    # Add other order-related fields as needed
